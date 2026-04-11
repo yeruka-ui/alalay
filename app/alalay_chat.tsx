7e25c42c371f40923625862c0037fb1647889a7f
yeruka-ui <laurcodes19@gmail.com>
2026-04-11 21:47:25 +0800

    Chat Panel is now viewed on the bottom of the screen.

--- app/alalay_chat.tsx
+++ app/alalay_chat.tsx
@@ -1,7 +1,8 @@
 import { router } from "expo-router";
 import React from "react";
 import { Text, TouchableOpacity, View } from "react-native";
-import { styles } from "../styles/index.styles";
+import { styles } from "../styles/alalay_chat.styles";
+import { styles as sharedStyles } from "../styles/index.styles";
 
 export default function AlalayChat() {
   return (
@@ -13,17 +14,23 @@ export default function AlalayChat() {
           justifyContent: "space-between",
         }}
       >
+        {/*Located here is the Top Panel: Consists of the back button and purple panel*/}
         <View>
-          <View style={styles.topPanel}></View>
+          <View style={sharedStyles.topPanel}></View>
 
-          <View style={styles.secondPanel}>
+          <View style={sharedStyles.secondPanel}>
             <TouchableOpacity
-              style={styles.backButton}
+              style={sharedStyles.backButton}
               onPress={() => router.back()}
             >
-              <Text style={styles.backArrow}>‹</Text>
+              <Text style={sharedStyles.backArrow}>‹</Text>
             </TouchableOpacity>
           </View>
+        </View>
+
+        {/*Located here is the Chat Panel: Consists of the chat messages and input field*/}
+        <View>
+          <View style={styles.chatPanel}></View>
         </View>
       </View>
     </>
---
+++ styles/alalay_chat.styles.ts
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  chatPanel: {
+    backgroundColor: "#ffffff",
+    borderRadius: 24,
+    padding: 20,
+    margin: 25,
+    shadowColor: "#00000084",
+    shadowOpacity: 0.1,
+    shadowRadius: 10,
+    elevation: 5,
+  },
+});
